3270f0ed57f061b2db438d1543c30cecd7ed503a
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-09-14 14:11:39 +0300

    starting play with springboot

---
+++ Section01-Spring Boot intro/src/main/java/com/springbootintro/section01springbootintro/Section01SpringBootIntroApplication.java
@@ -0,0 +1,13 @@
+package com.springbootintro.section01springbootintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section01SpringBootIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section01SpringBootIntroApplication.class, args);
+    }
+
+}
---
+++ Section01-Spring Boot intro/src/test/java/com/springbootintro/section01springbootintro/Section01SpringBootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootintro.section01springbootintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section01SpringBootIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
