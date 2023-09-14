24de899bcf01d2d6bcb24c8a3a6deca7b9e95749
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-09-14 15:14:47 +0300

    test restcontroller and test dev-tool

---
+++ Section01-Spring Boot intro/src/main/java/com/springbootintro/section01springbootintro/rest/FunRestController.java
@@ -0,0 +1,24 @@
+package com.springbootintro.section01springbootintro.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDaliyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+
+}
