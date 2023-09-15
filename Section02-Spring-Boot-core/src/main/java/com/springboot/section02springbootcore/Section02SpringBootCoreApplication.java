16fe2395a517cb679a78303418821b444de45b2e
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-09-15 06:26:58 +0300

    get ready to second section

---
+++ Section02-Spring-Boot-core/src/main/java/com/springboot/section02springbootcore/Section02SpringBootCoreApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.section02springbootcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section02SpringBootCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section02SpringBootCoreApplication.class, args);
+    }
+
+}
