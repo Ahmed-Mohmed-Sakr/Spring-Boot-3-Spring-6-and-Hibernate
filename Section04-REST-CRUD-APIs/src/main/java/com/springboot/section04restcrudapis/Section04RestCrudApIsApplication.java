bce13b48d39affb3951e9844fb32fcadad65d7ae
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-09-22 06:50:08 +0300

    undersant jackson and how it works in background and uderstand JSON struct

---
+++ Section04-REST-CRUD-APIs/src/main/java/com/springboot/section04restcrudapis/Section04RestCrudApIsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.section04restcrudapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section04RestCrudApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section04RestCrudApIsApplication.class, args);
+    }
+
+}
---
+++ Section04-REST-CRUD-APIs/src/test/java/com/springboot/section04restcrudapis/Section04RestCrudApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.section04restcrudapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section04RestCrudApIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
