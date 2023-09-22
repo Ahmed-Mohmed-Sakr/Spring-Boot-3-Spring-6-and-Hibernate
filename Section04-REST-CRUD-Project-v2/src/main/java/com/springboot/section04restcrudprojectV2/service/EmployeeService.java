c37ab900f25febb1db5ca9c892083f0d67511978
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-09-22 19:35:04 +0300

    understand data-rest and how it works

--- Section04-REST-CRUD-Project-v2/src/main/java/com/springboot/section04restcrudprojectV2/service/EmployeeService.java
+++ Section04-REST-CRUD-Project-v2/src/main/java/com/springboot/section04restcrudprojectV2/service/EmployeeService.java
@@ -1,15 +1,15 @@
-package com.springboot.section04restcrudprojectV2.service;
-
-import com.springboot.section04restcrudprojectV2.entity.Employee;
-
-import java.util.List;
-
-public interface EmployeeService {
-    List<Employee> findAll();
-
-    Employee findById(int theId);
-
-    Employee save(Employee theEmployee);
-
-    void deleteById(int theId);
-}
+//package com.springboot.section04restcrudprojectV2.service;
+//
+//import com.springboot.section04restcrudprojectV2.entity.Employee;
+//
+//import java.util.List;
+//
+//public interface EmployeeService {
+//    List<Employee> findAll();
+//
+//    Employee findById(int theId);
+//
+//    Employee save(Employee theEmployee);
+//
+//    void deleteById(int theId);
+//}
--- Section04-REST-CRUD-Project-v2/src/main/java/com/springboot/section04restcrudprojectV2/service/Impl/EmployeeServiceImpl.java
+++ Section04-REST-CRUD-Project-v2/src/main/java/com/springboot/section04restcrudprojectV2/service/Impl/EmployeeServiceImpl.java
@@ -1,49 +1,59 @@
-package com.springboot.section04restcrudprojectV2.service.Impl;
+/*
+no longer needed after this dependancy
 
-import com.springboot.section04restcrudprojectV2.dao.EmployeeRepository;
-import com.springboot.section04restcrudprojectV2.entity.Employee;
-import com.springboot.section04restcrudprojectV2.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+<dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-data-rest</artifactId>
+        </dependency>
 
-import java.util.List;
-import java.util.Optional;
+ */
 
-@Service
-public class EmployeeServiceImpl implements EmployeeService {
-
-    private EmployeeRepository employeeRepository;
-
-    @Autowired
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
-
-    @Override
-    public List<Employee> findAll() {
-        return employeeRepository.findAll();
-    }
-
-    @Override
-    public Employee findById(int theId) {
-        Optional<Employee> result = employeeRepository.findById(theId);
-        Employee theEmployee = null;
-
-        if (result.isPresent())
-            theEmployee = result.get();
-        else
-            throw new RuntimeException("Did not find employee id - " + theId);
-
-        return theEmployee;
-    }
-
-    @Override
-    public Employee save(Employee theEmployee) {
-        return employeeRepository.save(theEmployee);
-    }
-
-    @Override
-    public void deleteById(int theId) {
-        employeeRepository.deleteById(theId);
-    }
-}
+//package com.springboot.section04restcrudprojectV2.service.Impl;
+//
+//import com.springboot.section04restcrudprojectV2.dao.EmployeeRepository;
+//import com.springboot.section04restcrudprojectV2.entity.Employee;
+//import com.springboot.section04restcrudprojectV2.service.EmployeeService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class EmployeeServiceImpl implements EmployeeService {
+//
+//    private EmployeeRepository employeeRepository;
+//
+//    @Autowired
+//    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+//        this.employeeRepository = employeeRepository;
+//    }
+//
+//    @Override
+//    public List<Employee> findAll() {
+//        return employeeRepository.findAll();
+//    }
+//
+//    @Override
+//    public Employee findById(int theId) {
+//        Optional<Employee> result = employeeRepository.findById(theId);
+//        Employee theEmployee = null;
+//
+//        if (result.isPresent())
+//            theEmployee = result.get();
+//        else
+//            throw new RuntimeException("Did not find employee id - " + theId);
+//
+//        return theEmployee;
+//    }
+//
+//    @Override
+//    public Employee save(Employee theEmployee) {
+//        return employeeRepository.save(theEmployee);
+//    }
+//
+//    @Override
+//    public void deleteById(int theId) {
+//        employeeRepository.deleteById(theId);
+//    }
+//}
