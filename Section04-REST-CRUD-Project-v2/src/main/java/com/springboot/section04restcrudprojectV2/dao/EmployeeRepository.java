b8d319e367228767b5ccfa3d74fc498dd1ea9eb5
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-09-22 19:47:38 +0300

    test changing RepositoryRestResource and test ?page and ?sort properties

--- Section04-REST-CRUD-Project-v2/src/main/java/com/springboot/section04restcrudprojectV2/dao/EmployeeRepository.java
+++ Section04-REST-CRUD-Project-v2/src/main/java/com/springboot/section04restcrudprojectV2/dao/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.springboot.section04restcrudprojectV2.dao;
 
 import com.springboot.section04restcrudprojectV2.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
+//@RepositoryRestResource(path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that is every thing.!!!!!!1
