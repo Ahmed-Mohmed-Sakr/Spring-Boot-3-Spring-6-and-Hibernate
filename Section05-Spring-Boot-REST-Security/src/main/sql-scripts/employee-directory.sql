4fc750c02723a5e6d98b9cd392e06acd25622ccc
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-09-23 10:50:19 +0300

    setup my sql quaries

--- Section05-Spring-Boot-REST-Security/src/main/sql-scripts/employee-directory.sql
+++ Section05-Spring-Boot-REST-Security/src/main/sql-scripts/employee-directory.sql
@@ -20,9 +20,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
+	(1,'ahmed','sakr','ahmed@gmail.com'),
+	(2,'ali','sakr','ali@gmail.com'),
+	(3,'nader','ali','nader@gmail.com'),
+	(4,'mohmed','sakr','mohmed@gmail.com'),
+	(5,'walid','ali','walid@gmail.com');
 
