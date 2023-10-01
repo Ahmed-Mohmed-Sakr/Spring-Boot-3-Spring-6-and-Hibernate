9229fbcc514fe495de172550560887d24cf0ccb5
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-10-01 10:30:09 +0300

    starting with advanced topics on jpa/hibernates

---
+++ Section09-JPA_Hibernates_Advanced/src/main/java/com/springboot/section09jpa_hibernates_advanced/Section09JpaHibernatesAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.section09jpa_hibernates_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section09JpaHibernatesAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section09JpaHibernatesAdvancedApplication.class, args);
+	}
+
+}
---
+++ Section09-JPA_Hibernates_Advanced/src/test/java/com/springboot/section09jpa_hibernates_advanced/Section09JpaHibernatesAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.section09jpa_hibernates_advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section09JpaHibernatesAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
